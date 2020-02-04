39f22003be8128ea1e7f5582700fc91e5864090a
DCCA <dcca12@gmail.com>
2020-02-03 16:06:24 -0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp'),
+	terser = require('gulp-terser'),
+	rename = require('gulp-rename'),
+	browserSync = require('browser-sync').create(),
+	sass = require('gulp-sass'),
+	eslint = require('gulp-eslint');
+
+gulp.task('script', function() {
+	return gulp
+		.src('./js/*.js') // What files do we want gulp to consume?
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(terser()) // Call the terser function on these files
+		.pipe(rename({ extname: '.min.js' })) // Rename the uglified file
+		.pipe(gulp.dest('./build/js')); // Where do we put the result?
+});
+
+gulp.task('scss', function() {
+	return gulp
+		.src('./scss/*.scss') // What files do we want gulp to consume?
+		.pipe(sass()) // Call the scss function on these files
+		.pipe(rename({ extname: '.min.css' })) // Rename the scss file
+		.pipe(gulp.dest('./build/css')); // Where do we put the result?
+});
+
+gulp.task('reload', function(done) {
+	browserSync.reload();
+	done();
+});
+
+gulp.task('watch', function() {
+	browserSync.init({
+		server: './'
+	});
+	gulp.watch('./js/*.js', gulp.series('script', 'reload'));
+	gulp.watch('./scss/*.scss', gulp.series('scss', 'reload'));
+	gulp.watch('./index.html', gulp.series('reload'));
+});
+
+gulp.task('default', gulp.series('script', 'scss', 'watch', 'reload'));
