f1c96ba1094dc6dc2dc31f083619ef0836064d12
DCCA <dcca12@gmail.com>
2020-02-04 12:51:16 -0800

    setup createCard function

--- js/script.js
+++ js/script.js
@@ -35,6 +35,7 @@ document.addEventListener(
 
 		// Get elements in the HTML
 		const sectionSelector = document.getElementById('section');
+		const cardSection = document.querySelector('.card-section');
 
 		sectionList.forEach(e => {
 			const optionElement = document.createElement('option');
@@ -53,11 +54,33 @@ document.addEventListener(
 			fetch(
 				`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${API_KEY}`
 			)
-				.then(res => console.log(res))
-				.catch();
+				.then(res => res.json())
+				.then(data => {
+					console.log(data.section);
+					console.log(data.results);
+				})
+				.catch(err => {
+					console.log(err);
+				});
+		}
+
+		// Create card
+		function createCard(parentElement, imgUrl, abstract) {
+			// Create div
+			const div = document.createElement('div');
+			div.setAttribute('class', 'card');
+			div.style.backgroundImage = `url(${imgUrl})`;
+			// Create abstract
+			const p = document.createElement('p');
+			p.setAttribute('class', abstract);
+			p.innerText = abstract;
+			// Append elements
+			div.appendChild(p);
+			parentElement.appendChild(div);
 		}
 
 		getStories('arts');
+		createCard(cardSection, 'https://picsum.photos/640/360', 'Teste');
 
 		// END - Wrapper for DOM ready
 	},
