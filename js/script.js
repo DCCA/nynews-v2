e35298d53f3d1f9b30ee5fc1dbc50254ab0ad9fb
DCCA <dcca12@gmail.com>
2020-02-04 12:19:02 -0800

    setup the API key with config.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,65 @@
-console.log('conected');
+import { API_KEY } from '../../config.js';
+
+document.addEventListener(
+	'DOMContentLoaded',
+	function() {
+		//START - Wrapper for DOM ready
+		const sectionList = [
+			'arts',
+			'automobiles',
+			'books',
+			'business',
+			'fashion',
+			'food',
+			'health',
+			'home',
+			'insider',
+			'magazine',
+			'movies',
+			'nyregion',
+			'obituaries',
+			'opinion',
+			'politics',
+			'realestate',
+			'science',
+			'sports',
+			'sundayreview',
+			'technology',
+			'theater',
+			't - magazine',
+			'travel',
+			'upshot',
+			'us',
+			'world'
+		];
+
+		// Get elements in the HTML
+		const sectionSelector = document.getElementById('section');
+
+		sectionList.forEach(e => {
+			const optionElement = document.createElement('option');
+			optionElement.innerText = uppercaseFirstLetter(e);
+			optionElement.value = e;
+			sectionSelector.appendChild(optionElement);
+		});
+
+		// To Upper Case
+		function uppercaseFirstLetter(str) {
+			return str.charAt(0).toUpperCase() + str.slice(1);
+		}
+
+		// API Call
+		function getStories(section) {
+			fetch(
+				`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${API_KEY}`
+			)
+				.then(res => console.log(res))
+				.catch();
+		}
+
+		getStories('arts');
+
+		// END - Wrapper for DOM ready
+	},
+	false
+);
